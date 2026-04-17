18e437266323e2c88e990a82242c398fdc5b5732
amanzhola <amanzholaimov@gmail.com>
2026-04-17 21:37:27 +0500

    feat: implement async HTTP server solution

---
+++ sprint1/problems/async_server/solution/src/main.cpp
@@ -0,0 +1,122 @@
+#include "sdk.h"
+
+#include <boost/asio/signal_set.hpp>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include "http_server.h"
+
+namespace {
+namespace net = boost::asio;
+using namespace std::literals;
+namespace sys = boost::system;
+namespace http = boost::beast::http;
+
+// Запрос, тело которого представлено в виде строки
+using StringRequest = http::request<http::string_body>;
+// Ответ, тело которого представлено в виде строки
+using StringResponse = http::response<http::string_body>;
+
+struct ContentType {
+    ContentType() = delete;
+    constexpr static std::string_view TEXT_HTML = "text/html"sv;
+};
+
+// Создаёт StringResponse с заданными параметрами
+StringResponse MakeStringResponse(http::status status,
+                                  std::string_view body,
+                                  unsigned http_version,
+                                  bool keep_alive,
+                                  std::string_view content_type = ContentType::TEXT_HTML) {
+    StringResponse response(status, http_version);
+    response.set(http::field::content_type, content_type);
+    response.body() = body;
+    response.content_length(body.size());
+    response.keep_alive(keep_alive);
+    return response;
+}
+
+StringResponse HandleRequest(StringRequest&& req) {
+    const auto make_ok_response = [&req](std::string_view body) {
+        return MakeStringResponse(http::status::ok,
+                                  body,
+                                  req.version(),
+                                  req.keep_alive(),
+                                  ContentType::TEXT_HTML);
+    };
+
+    const auto make_invalid_method_response = [&req]() {
+        auto response = MakeStringResponse(http::status::method_not_allowed,
+                                           "Invalid method."sv,
+                                           req.version(),
+                                           req.keep_alive(),
+                                           ContentType::TEXT_HTML);
+        response.set(http::field::allow, "GET, HEAD");
+        return response;
+    };
+
+    if (req.method() != http::verb::get && req.method() != http::verb::head) {
+        return make_invalid_method_response();
+    }
+
+    std::string target = std::string(req.target());
+
+    if (!target.empty() && target.front() == '/') {
+        target.erase(target.begin());
+    }
+
+    std::string body = "Hello, "s + target;
+
+    auto response = make_ok_response(body);
+
+    if (req.method() == http::verb::head) {
+        response.body().clear();
+        response.content_length(body.size());
+    }
+
+    return response;
+}
+
+// Запускает функцию fn на n потоках, включая текущий
+template <typename Fn>
+void RunWorkers(unsigned n, const Fn& fn) {
+    n = std::max(1u, n);
+    std::vector<std::jthread> workers;
+    workers.reserve(n - 1);
+    while (--n) {
+        workers.emplace_back(fn);
+    }
+    fn();
+}
+
+}  // namespace
+
+int main() {
+    const unsigned num_threads = std::max(1u, std::thread::hardware_concurrency());
+
+    net::io_context ioc(static_cast<int>(num_threads));
+
+    // Подписываемся на сигналы и при их получении завершаем работу сервера
+    net::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait([&ioc](const sys::error_code& ec, [[maybe_unused]] int signal_number) {
+        if (!ec) {
+            ioc.stop();
+        }
+    });
+
+    const auto address = net::ip::make_address("0.0.0.0");
+    constexpr net::ip::port_type port = 8080;
+
+    http_server::ServeHttp(ioc, {address, port}, [](auto&& req, auto&& sender) {
+        sender(HandleRequest(std::forward<decltype(req)>(req)));
+    });
+
+    // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
+    std::cout << "Server has started..."sv << std::endl;
+
+    RunWorkers(num_threads, [&ioc] {
+        ioc.run();
+    });
+}
