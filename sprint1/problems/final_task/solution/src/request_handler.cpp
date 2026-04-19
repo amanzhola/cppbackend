05699163fd3585b6d8cf8740fab0e7be7bd21733
amanzhola <amanzholaimov@gmail.com>
2026-04-19 20:32:56 +0000

    fix: address review comments and restore template handler definition

--- sprint1/problems/final_task/solution/src/request_handler.cpp
+++ sprint1/problems/final_task/solution/src/request_handler.cpp
@@ -1,23 +1,6 @@
 #include "request_handler.h"
 
-#include <string>
-
 namespace http_handler {
-
-using namespace std::literals;
-
-namespace {
-
-constexpr std::string_view kApiPrefix = "/api/"sv;
-constexpr std::string_view kMapsEndpoint = "/api/v1/maps"sv;
-constexpr std::string_view kMapsEndpointPrefix = "/api/v1/maps/"sv;
-
-constexpr std::string_view kBadRequestCode = "badRequest"sv;
-constexpr std::string_view kBadRequestMessage = "Bad request"sv;
-constexpr std::string_view kMapNotFoundCode = "mapNotFound"sv;
-constexpr std::string_view kMapNotFoundMessage = "Map not found"sv;
-
-}  // namespace
 
 RequestHandler::RequestHandler(model::Game& game)
     : game_{game} {
@@ -123,41 +106,6 @@ json::value RequestHandler::SerializeMap(const model::Map& map) const {
     obj["offices"] = std::move(offices);
 
     return obj;
-}
-
-template <typename Body, typename Allocator, typename Send>
-void RequestHandler::operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
-    const std::string target = std::string(req.target());
-    const unsigned version = req.version();
-    const bool keep_alive = req.keep_alive();
-
-    if (req.method() != http::verb::get) {
-        return send(MakeBadRequest(version, keep_alive, kBadRequestCode, kBadRequestMessage));
-    }
-
-    if (target == kMapsEndpoint) {
-        return send(MakeJsonResponse(version, keep_alive, SerializeMaps()));
-    }
-
-    if (target.starts_with(kMapsEndpointPrefix)) {
-        const std::string map_id = target.substr(kMapsEndpointPrefix.size());
-
-        if (map_id.empty()) {
-            return send(MakeBadRequest(version, keep_alive, kBadRequestCode, kBadRequestMessage));
-        }
-
-        if (const model::Map* map = game_.FindMap(model::Map::Id(map_id))) {
-            return send(MakeJsonResponse(version, keep_alive, SerializeMap(*map)));
-        }
-
-        return send(MakeNotFound(version, keep_alive, kMapNotFoundCode, kMapNotFoundMessage));
-    }
-
-    if (target.starts_with(kApiPrefix)) {
-        return send(MakeBadRequest(version, keep_alive, kBadRequestCode, kBadRequestMessage));
-    }
-
-    return send(MakeBadRequest(version, keep_alive, kBadRequestCode, kBadRequestMessage));
 }
 
 }  // namespace http_handler
