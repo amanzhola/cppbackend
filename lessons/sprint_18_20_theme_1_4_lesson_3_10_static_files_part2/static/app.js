acadb77f3e9411d642ae6613affa8208e5f8fb52
amanzhola <amanzholaimov@gmail.com>
2026-04-30 03:10:07 +0500

    Remove mistaken lessons and restore README on main

--- lessons/sprint_18_20_theme_1_4_lesson_3_10_static_files_part2/static/app.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById("game");
-const context = canvas.getContext("2d");
-
-context.fillStyle = "lime";
-context.fillRect(100, 100, 80, 80);
-
-console.log("Static files lesson started");
